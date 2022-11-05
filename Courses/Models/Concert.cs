6af09429bfeaba02e4aec51d23f650ee56b433fa
thatshowmafiaworks <shinehd56@gmail.com>
2022-11-05 20:55:06 +0200

    added basic models

---
+++ Courses/Models/Concert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Models
+{
+    public class Concert
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Artist { get; set; } = String.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Courses/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Courses.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public Guid ConcertId { get; set; }
+        public string Place { get; set; } = String.Empty;
+        public TimeOnly BeginningDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
